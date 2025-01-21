191e98d022462bc94d2873dcfa5aea6fe478eb35
Ravindu Sadaruwan <sravindu2005@gmail.com>
2025-01-21 19:19:43 +0530

    airavindu

---
+++ sample.js
@@ -0,0 +1,7 @@
+console.log('Hello, World!');
+confirm('Hello, World!');
+
+ane HTMLUListElement object, which is a list of all the elements
+KeyframeEffectkjf
+KeyframeEffectList
+KeyframeEffectOptionsflfl
